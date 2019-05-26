736b3f1aebaf8329dbdd63d596df7e63258b7810
Jason Moore <jason.moore1369@gmail.com>
2019-05-26 13:13:47 -0500

    corrected seeds.sql to align with instructions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,4 @@
-USE burgers_db;
-
 INSERT INTO burgers (burger_name, devoured) VALUES ("The Biggums", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Pile O' Meat", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Number J", false);
 
-SELECT * FROM burgers_db.burgers;
