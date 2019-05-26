676c34becdf0137c46b7b5e865f9588c80b0f120
Jason Moore <jason.moore1369@gmail.com>
2019-05-26 13:11:09 -0500

    seeds.sql now populates 3 initial burgers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,7 @@
---init commit--
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Biggums", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Pile O' Meat", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Number J", false);
+
+SELECT * FROM burgers_db.burgers;
