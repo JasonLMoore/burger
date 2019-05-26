69a80851f15cb2c6537847470e2d3ce8c2cd34c9
Jason Moore <jason.moore1369@gmail.com>
2019-05-26 12:50:52 -0500

    schema for creating burger_db and burgers TABLE created and tested

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,12 @@
 --init commit--
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers {
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(225),
+    devoured BOOLEAN
+    PRIMARY KEY(id)
+};
