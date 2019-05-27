8e4fb683212be087b4f52d02b19d6ba3ca7e9490
Jason Moore <jason.moore1369@gmail.com>
2019-05-27 13:02:14 -0500

    installed mysql npm and set up connection in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,20 @@
 //init commit//
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Yellow13",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
