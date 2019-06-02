f077649f22b134dd570653bec8b4869e20d39ab7
Jason Moore <jason.moore1369@gmail.com>
2019-06-02 13:57:22 -0500

    fixed minor syntax error in server.js (express needed '---')

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require(express);
+const express = require("express");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/burgers_controller.js");
 
